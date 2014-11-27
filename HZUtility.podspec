93760357413290770f560364e7de0e5134cd8583
hzwzw <hzwzw@hzwzws-iMac.local>
2014-11-27 14:03:48 +0800

    add uiview size class

---
+++ HZUtility.podspec
@@ -0,0 +1,15 @@
+Inkash.xcworkspace/xcshareddata/Inkash.xccheckout
+d::Spec.new do |s|
+  s.name         = 'HZUtility'
+  s.summary      = 'something useful utility class for iOS dev'
+  s.homepage     = 'https://github.com/hzwzw/HZUtility'
+  s.license      = 'MIT'
+
+  s.source       = { :git => 'git@github.com:hzwzw/HZUtility.git'}
+  s.platform     = :ios, '5.0'
+
+  s.source_files = 'UIView+Sizes.{h,m}'
+
+  s.requires_arc = true
+end
+
---
+++ UIView/UIView+Sizes.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Sizes.h
+//  Scott
+//
+//  Created by mengjq on 4/4/14.
+//  Copyright (c) 2014 Jianqiang Meng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Sizes)
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize size;
+@end
